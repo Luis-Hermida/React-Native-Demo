6ead2d888418c4ae59406c631d20e6d62f971f28
Luis Alejandro Hermida <luis.hn@zoho.com>
2020-07-01 21:31:54 -0500

    Added android package and heroku API

--- assets/js/baseUrl.js
+++ assets/js/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = "http://192.168.100.101:3001/";
+// export const baseUrl = "http://192.168.100.101:3001/";
+export const baseUrl = "https://react-restaurant-lahn.herokuapp.com/";
